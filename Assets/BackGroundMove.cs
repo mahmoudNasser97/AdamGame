9c69ed139120df7317a4099a557d830721285189
mahmoudNasser97 <59306439+mahmoudNasser97@users.noreply.github.com>
2024-11-04 20:50:25 +0200

    Adding New Ability move

---
+++ Assets/BackGroundMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BackGroundMove : MonoBehaviour
+{
+    public float moveDistance = 10f;
+    public float duration = 5f;
+    public Ease easeEffect;
+
+    void Start()
+    {
+        // Move left and right continuously
+        transform.DOMoveX(transform.position.x + moveDistance, duration)
+            .SetEase(easeEffect)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
